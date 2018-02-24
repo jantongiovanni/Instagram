4c3cd338cc661a3b7361e246096d6eb44fbbccc0
Joe Antongiovanni <joe@Joes-Mac.local>
2018-02-23 19:17:29 -0800

    image picker

--- Instagram/ComposeViewController.swift
+++ Instagram/ComposeViewController.swift
@@ -8,20 +8,45 @@
 
 import UIKit
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var tap: UITapGestureRecognizer!
+    let vc = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        vc.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = originalImage
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        print("tap")
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
     }
     
     @IBAction func onCancel(_ sender: AnyObject) {
